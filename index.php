aed99687f567e1949cb141ef107982fb52e8f33f
JossRV <jossrv74@gmail.com>
2025-04-13 19:31:39 -0600

    cambio de estilo bootstrap a tailwind

--- index.php
+++ index.php
@@ -28,47 +28,30 @@ curl_close($ch);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La próxima pelicula de Marvel</title>
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body class="bg-dark">
+<body class="bg-violet-950">
     <!-- aplicacion -->
     <main>
         <section>
-            <div class="container my-4">
-                <div class="row">
-                    <div class="col text-center">
-                        <h2>La proxima pelicula de Marvel</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- <pre>
+            <div class="grid mx-auto gap-5 text-center">
+                <!-- <pre>
                         <?= var_dump($data); ?>
                     </pre> -->
-                    <div class="col"></div>
-                    <div class="col-8">
-                        <div class="card" style="max-width: 740px;">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img class="img-fluid rounded-start" src="<?= $data['poster_url'] ?>" alt="Poster de <?= $data['title'] ?>" />
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-center"><?= $data['title'] ?> se estrena en <?= $data['days_until'] ?> días</h5>
-                                        <p class="card-text">Fecha de estreno <?= $data['release_date'] ?></p>
-                                        <p class="card-text">La siguiente pelicula es <?= $data['following_production']['title'] ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col"></div>
-                </div>
+                <!-- <div class="grid mx-auto gap-5"> -->
+                <h2>La proxima pelicula de Marvel</h2>
+                <img class="mx-auto w-sm rounded-2" src="<?= $data['poster_url'] ?>"
+                    alt="Poster de <?= $data['title'] ?>" />
+                <p><?= $data['title'] ?> se estrena en <?= $data['days_until'] ?> días</p>
+                <p class="card-text">Fecha de estreno <?= $data['release_date'] ?></p>
+                <p class="card-text">
+                    La siguiente pelicula es <?= $data['following_production']['title'] ?>
+                </p>
+                <!-- </div> -->
             </div>
         </section>
     </main>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
 </html>
